681f71d5df7abc80133c45c8639912c36d6a5839
Matthew <denicematthewz@gmail.com>
2023-04-22 21:42:59 +0700

    tugas 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,7 +21,7 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
         $this->call(UserTableSeeder::class);
-
+        $this->call(PostsTableSeeder::class);
 
     }
 }
